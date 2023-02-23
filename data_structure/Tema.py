2a72f11f1c14d85eb6f0ed724acf2cee3a45f8c2
OvidiuOne <cioflan.ovidiu@gmail.com>
2023-02-23 15:40:16 +0200

    ddd

--- data_structure/Tema.py
+++ data_structure/Tema.py
@@ -11,5 +11,10 @@ print("Numerele pare:", even_numbers)
 odd_numbers = sorted(numbers)[::2]
 print("Numerele impare:", odd_numbers)
 
+#multiplu ai nr 3 - lista aranjata
 multiples_of_3 = sorted(numbers)[2::3]
 print("Multiplu de 3:", multiples_of_3)
+
+#multiplu ai nr 3 - lista nearanjata
+multiples_3 = [x for x in numbers if x % 3 == 0]
+print("Multiplu de 3 nearanjat", multiples_3)
